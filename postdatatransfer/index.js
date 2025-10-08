ff20f84ab9ab9e799381a3186fbb03659315b346
Logezz-07 <logeshwaran.b@servion.com>
2025-10-08 21:42:23 +0530

    new change

--- postdatatransfer/index.js
+++ postdatatransfer/index.js
@@ -1,135 +1,90 @@
 const functions = require("@google-cloud/functions-framework");
 const apiRequest = require('@roger/r4b-common-nodemodules').apiClient;
 const logger = require('@roger/r4b-common-nodemodules').logger;
-const axios = require("axios");
 
 functions.http("helloHttp", async (req, res) => {
-  const sessionId = req.body.sessionInfo?.session.split("/sessions/").pop() || "unknown-session";
-  const tag = req.body.fulfillmentInfo?.tag || "Unknown-Tag";
+    const sessionId = req.body.sessionInfo?.session?.split("/sessions/").pop() || "unknown-session";
+    const tag = req.body.fulfillmentInfo?.tag || "PostDataTransfer";
+    logger.logWebhookDetails(sessionId, tag);
+    const sessionParamsFromCX = req.body.sessionInfo?.parameters || {};
+    let Status = 1;
+    let ResponsePayload = {};
 
-  logger.logWebhookDetails(sessionId, tag);
-  const sessionParamsFromCX = req.body.sessionInfo?.parameters || {};
-  let sessionParams = {};
-  let Status = 1;
-  let ResponsePayload = {};
-
-  try {
-    // ---------------------- CallStartApi ---------------------- //
-    if (tag === "callStartApi") {
-      const dnis = sessionParamsFromCX?.Dnis || "NA";
-      logger.logWebhookRequest(sessionId, tag, { Dnis: dnis });
-
-      const apiUrl = `https://dev1-cct.rogers.com/dev01-config/ivr-config-ms/ivr/${dnis}/call-start/params`;
-      const headers = {
-        cdr: "112432245667775757",
-        clientSystem: "IVR",
-        brand: "FIDO",
-        transactionId: "454545323",
-        transactionDateTime: "2024-11-07T10:23:00",
-        ivrSubscriptionKey: "f9422c1450c747aaaca69253a489f3c6",
-      };
-
-      const apiResult = await apiRequest.getRequest({ sessionId, tag, url: apiUrl, headers });
-      Status = apiResult.Status;
-      ResponsePayload = apiResult.ResponsePayload;
-
-      if (Status === 200 && apiResult.ReturnCode === "0") {
-        const dnisParams = ResponsePayload.dnisParams || {};
-        const aniParams = ResponsePayload.aniParams || {};
-        sessionParams = {
-          SearchBusinessContact: dnisParams.icmSearch?.searchBusinessContact || "NA",
-          HomeContact: dnisParams.icmSearch?.searchHomeContact || "NA",
-          MobileContact: dnisParams.icmSearch?.searchMobileContact || "NA",
-          aniConfirm: dnisParams.aniConfirm || "NA",
-          aniLookup: dnisParams?.aniLookup || "NA",
-          dnisLang: dnisParams.language?.offerLanguageMenu || "NA",
-          identifyAccount: dnisParams.identifyAccount || "NA",
-          npaLanguage: aniParams.npaLanguage || "NA",
-          offerlanguageMenu: dnisParams.language?.offerLanguageMenu || "NA",
-          predctiveEnd: dnisParams.predictiveInd || "NA",
-          validAni: aniParams.validANI || "NA",
-          returnCode: "0",
+    try {
+        // Secret headers from request
+        const secretHeader = {
+            token: req.headers["token"],
+            expiryTime: req.headers["expirytime"],
+            clientId: req.headers["clientid"],
+            clientSecret: req.headers["clientsecret"]
         };
-      } else {
-        sessionParams = {
-          returnCode: apiResult.ReturnCode || "1",
-          response: ResponsePayload,
+
+        if (!secretHeader.token || !secretHeader.expiryTime || !secretHeader.clientId || !secretHeader.clientSecret) {
+            throw new Error("Missing required secret headers");
+        }
+        const transferId = sessionParamsFromCX?.CONN_ID || "1234567890abcdef";
+        const callingSystem = req.query?.callingSystem || "engage";
+
+        const apiUrl = `https://dev01-cct.rogers.com/dev01-cctdtapi/data-transfer/1.0.0/datatransfer/${transferId}?callingSystem=${callingSystem}`;
+
+        // Prepare headers
+        const headers = {
+            "Content-Type": "application/json",
+            cdr: "112432245667775757",
+            clientSystem: "IVR",
+            brand: "FIDO",
+            transactionId: "454545323",
+            transactionDateTime: new Date().toISOString(),
+            ivrSubscriptionKey: "f9422c1450c747aaaca69253a489f3c6"
         };
-      }
+
+        // Prepare body payload
+        const payload = [
+            { keyName: "svcReferenceId", transformType: "any", value: sessionParamsFromCX?.svcReferenceId || transferId },
+            { keyName: "AGENT_ALERT", transformType: "any", value: sessionParamsFromCX?.AGENT_ALERT || "Default Alert" },
+            { keyName: "SemaphoneCR", transformType: "any", value: sessionParamsFromCX?.SemaphoneCR || "DefaultCR" },
+            { keyName: "ANI", transformType: "phoneNumber", value: sessionParamsFromCX?.ANI || "0000000000" },
+            { keyName: "LNG", transformType: "any", value: sessionParamsFromCX?.LNG || "en" },
+            { keyName: "CONN_ID", transformType: "any", value: transferId },
+            { keyName: "conversationId", transformType: "any", value: sessionParamsFromCX?.conversationId || transferId },
+            { keyName: "lastChkPt", transformType: "any", value: sessionParamsFromCX?.lastChkPt || "" }
+        ];
+
+        logger.logWebhookRequest(sessionId, tag, payload);
+
+        // Call API via apiClient (handles token internally)
+        const apiResult = await apiRequest.postRequest({ sessionId, tag, url: apiUrl, headers, data: payload, secretHeader });
+        Status = apiResult.Status;
+        ResponsePayload = apiResult.ResponsePayload;
+
+        // ---------------------- Prepare webhook response ---------------------- //
+        let sessionParams = {};
+        if (Status === 200 && apiResult.ReturnCode === "0") {
+            sessionParams = { returnCode: "0", message: ResponsePayload?.message };
+        } else {
+            sessionParams = { returnCode: apiResult.ReturnCode || "1", response: ResponsePayload };
+        }
+
+        const webhookResponse = { sessionInfo: { parameters: sessionParams } };
+        logger.logWebhookResponse(sessionId, tag, webhookResponse);
+
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(webhookResponse);
+
+    } catch (err) {
+        logger.logErrorResponse({ sessionId, tag, attempt: 1, err });
+
+        const webhookResponse = {
+            sessionInfo: {
+                parameters: {
+                    returnCode: "1",
+                    errorDetails: err.message || String(err)
+                }
+            }
+        };
+
+        logger.logWebhookResponse(sessionId, tag, webhookResponse);
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).send(webhookResponse);
     }
-
-    // ---------------------- AniIdentification ---------------------- //
-    else if (tag === "AniIdentification") {
-      const ani = sessionParamsFromCX?.Ani || "NA";
-      logger.logWebhookRequest(sessionId, tag, { Ani: ani });
-
-      const apiUrl = "https://dev1-cct.rogers.com/dev01-identification/ivr-identification-ms/idc/data";
-      const headers = {
-        cdr: "112432245667775757",
-        clientSystem: "IVR",
-        brand: "FIDO",
-        transactionId: "4545453232",
-        transactionDateTime: "2024-11-07T10:23:00",
-        ivrSubscriptionKey: "f9422c1450c747aaaca69253a489f3c6",
-      };
-
-      const params = {
-        brand: "FIDO",
-        searchHomeContact: "Y",
-        searchMobileContact: "Y",
-        searchBusinessContact: "Y",
-        phoneNumber: ani,
-        predictiveInd: "Y",
-      };
-
-      const apiResult = await apiRequest.getRequest({ sessionId, tag, url: apiUrl, headers, params });
-      Status = apiResult.Status;
-      ResponsePayload = apiResult.ResponsePayload;
-      if (Status === 200 && ResponsePayload.identifiedCustomer) {
-        const accountList = [];
-        ResponsePayload.idcData?.primaryContacts?.forEach((contact) => {
-          contact.customers?.forEach((customer) => {
-            customer.accounts?.forEach((account) => {
-              if (account.accountNumber) {
-                accountList.push(account.accountNumber.slice(-4));
-              }
-            });
-          });
-        });
-        sessionParams = {
-          returnCode: "0",
-          identifiedCustomer: ResponsePayload.identifiedCustomer,
-          AccountList: accountList,
-        };
-      
-      } else {
-        sessionParams = {
-          returnCode: "1",
-          response: ResponsePayload,
-        };
-      }
-    }
-
-    // ---------------------- Webhook Response ---------------------- //
-    const webhookResponse = { sessionInfo: { parameters: { ...sessionParams } } };
-    logger.logWebhookResponse(sessionId, tag, webhookResponse);
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).send(webhookResponse);
-  } catch (err) {
-    logger.logErrorResponse({ sessionId, tag, attempt: 1, err });
-
-    const returnCode = "1";
-    const errorDetails =
-      err.code === "ECONNABORTED"
-        ? "Request timed out after 15 seconds"
-        : err.message || String(err);
-
-    const webhookResponse = {
-      sessionInfo: { parameters: { returnCode, errorDetails } },
-    };
-
-    logger.logWebhookResponse(sessionId, tag, webhookResponse);
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).send(webhookResponse);
-  }
 });

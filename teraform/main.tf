a3617721d7c1392a48c65111ad52e3fe63c45e13
Logezz-07 <logeshwaran.b@servion.com>
2025-10-17 00:24:02 +0530

    upd

--- teraform/main.tf
+++ teraform/main.tf
@@ -38,7 +38,7 @@ data "archive_file" "functions" {
   type        = "zip"
   output_path = "/tmp/${each.key}.zip"
   source_dir  = "../${each.key}"  
-  excludes    = ["node_modules","README.md",".gitignore",".npmrc"]
+  excludes    = ["node_modules","README.md",".gitignore"]
 }
 
 # Upload each zip to bucket

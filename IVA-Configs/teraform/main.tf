4703b7e7777f194f276b0ab1585d387d21c66f30
Logezz-07 <logeshwaran.b@servion.com>
2025-11-06 22:25:09 +0530

    new chn

--- IVA-Configs/teraform/main.tf
+++ IVA-Configs/teraform/main.tf
@@ -1,19 +1,7 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 5.0.0"
-    }
-  }
-
-  backend "gcs" {}
-}
-
 provider "google" {
   project = var.project_id
 }
 
-# Upload the IVA config file to GCS
 resource "google_storage_bucket_object" "iva_config" {
   name         = "config.json"
   bucket       = var.bucket_name
@@ -21,7 +9,6 @@ resource "google_storage_bucket_object" "iva_config" {
   content_type = "application/json"
 }
 
-# Output the uploaded file name
 output "uploaded_file_name" {
   value = google_storage_bucket_object.iva_config.name
 }
